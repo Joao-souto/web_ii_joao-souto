b765c90b94ef5e5026b10cfdec5bd55cabfdac11
João Souto <joaosouto0@outlook.com>
2024-03-06 09:40:54 -0300

    exercicio 5

---
+++ exercicio4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Mãe</h1>";
+        echo "<h3>Sem palavras, a senhora é uma rainha!</h3>";
+    ?>
+</body>
+</html>
---
+++ exercicio5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+</head>
+<body>
+    <?php
+    $x = 6;
+    $y = 12;
+    $xy = $x + $y;
+    echo "Até então sei apenas mostrar mensagem e criar variáveis, exemplo: x = $x e y = $y. A soma de x e y é $xy.";
+    ?>
+</body>
+</html>
