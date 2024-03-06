6f49d462dcb63a71f3359c69206b9d5e62f636d6
João Souto <joaosouto0@outlook.com>
2024-03-06 09:24:57 -0300

    exercicio 3

--- exercicio2.php
+++ exercicio2.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 1</title>
+    <title>Exercício 2</title>
 </head>
 <body>
     <?php
-    echo "<h1>O primeiro programa a gente nunca esquece</h1>"
+    echo "<h1>João Pedro Monteiro Souto</h1>";
+    echo "<h3>Praça Alberto Lion</h3>";
+    echo "<h3>CEP: 01515-000 Num: 11 97653-5178</h3>";
     ?>
 </body>
 </html>
---
+++ exercicio3.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Corra - Djonga</h1>";
+
+    echo "<p>Amor, olha o que fizeram com nosso povo <br>
+    Amor, esse é o sangue da nossa gente <br>
+    Amor, olha a revolta do nosso povo <br>
+    Eu vou, juro que hoje eu vou ser diferente <br> </p>";
+
+    echo "<p> <br> Éramos milhões, até que vieram vilões<br>
+    O ataque nosso não bastou<br>
+    Fui de bastão, eles tinham a pólvora<br>
+    Vi meu povo se apavorar<br>
+    E às vezes eu sinto que nada que eu tente fazer vai mudar<br>
+    Auto estima é tipo confiança, só se quebra uma vez<br>
+    'To juntando os cacos, não Barcelos, nem Antibes<br>
+    Sou antigo na arte de nascer das cinza<br>
+    Tanto quanto um bom motorista, é<br>
+    Na arte de fazer baliza<br>
+    Eu 'to na arte de fazer<br>
+    Eles são a resposta pra fome<br>
+    Eles são o revólver que aponta<br>
+    Vocês são a resposta porque tanto Einstein no morro morre e não desponta<br>
+    Vocês são o meu medo na noite<br>
+    Vocês são mentira bem contada<br>
+    Vocês são a porra do sistema que vê mãe sofrendo e faz virar piada, porra<br>
+    Eu vi os menor pegando em arma, pois 'cês foram silenciadores<br>
+    Eu vi meu pai chorando o desemprego, desespero<br>
+    Pra que isso, mano?<br>
+    Eu não quero vida de pizzaiolo, e sim ser dono da pizzaria<br>
+    Querem que eu me contente com nada<br>
+    Sem meu povo tudo não existiria<br>
+    Eu disse óh como 'cê chega na minha terra<br>
+    Ele responde quem disse que a terra é sua?<br><br></p>";
+
+    echo "<p>Ô ô ô ô ô ô ô <br>
+    Ô ô ô ô ô ô ô<br><br></p>";
+
+    echo "<p>Aquela noite eu te ensinei coisas sobre o amor<br>
+    Durante o dia eu só tinha vivido o ódio<br>
+    Deus deu o frio e não me deu o cobertor<br>
+    Perdão Senhor, mas na pista eu só vejo sódio<br>
+    Se pá são a causa da seca, e da cerca que nos separa<br>
+    Depois nos acusam de 'tá dividindo demais<br>
+    Já se apropriaram de tudo<br>
+    Minha mente me diz get out Gustavo, corra<br>
+    Você sabe o mal que isso faz<br>
+    Pra eles nota seis é muito<br>
+    Pra nóis nota dez ainda é pouco<br>
+    Pros meus qualquer grana é o mundo<br>
+    Pros deles qualquer grana é troco<br>
+    E eu 'to errado antes de fazer, defasar é o prazer<br>
+    De quem 'tá com o controle do game<br>
+    Não treme, não geme, se cala vadia<br>
+    Aqui é a porra do senhor de engenho<br>
+    Eu sou tudo, eu sou vídeo, eu sou foto, eu sou frame<br>
+    Tem que se vender pra mim se tu quiser um Grammy<br>
+    Sou a morte, o diabo, o capeta<br>
+    A careta que te assombra quando fecha o olho<br>
+    Enquanto eles gozam com o choro<br>
+    Existirei pra fazer tu sorrir, amor<br>
+    Sou seu colete à prova de balas<br>
+    Seu ouvido à prova de falas<br>
+    Eu vou tomar nosso mundo de volta<br><br></p>";
+
+    echo "<p>Amor, olha o que fizeram com nosso povo <br>
+    Amor, esse é o sangue da nossa gente<br>
+    Amor, olha a revolta do nosso povo<br>
+    Eu vou, juro que hoje eu vou ser diferente<br></p>"
+    ?>
+</body>
+</html>
