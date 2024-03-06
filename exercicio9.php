05d1d13f9d12f50439d9571da6abf02474975c00
João Souto <joaosouto0@outlook.com>
2024-03-06 10:18:08 -0300

    exercicio 10

---
+++ exercicio9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <?php
+        echo "0 - Fim<br>";
+        echo "1 - Inclui<br>";
+        echo "2 - Altera<br>";
+        echo "3 - Exclui<br>";
+        echo "4 - Consulta<br>";
+    ?>
+</body>
+</html>
