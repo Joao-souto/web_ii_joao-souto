472e5d5d37851203893a10033bbd89010646fd98
João Souto <joaosouto0@outlook.com>
2024-03-06 09:50:28 -0300

    exercicio 6

---
+++ exercicio6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 6</title>
+    <style>
+        span {
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "xxxxx<br>";
+    echo "x<span>xxx</span>x<br>";
+    echo "x<span>xxx</span>x<br>";
+    echo "x<span>xxx</span>x<br>";
+    echo "xxxxx<br>";
+    ?>
+</body>
+</html>
