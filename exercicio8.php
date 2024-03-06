24d2de64a8e0939e4ec5bebc0aaac3270a8466e1
João Souto <joaosouto0@outlook.com>
2024-03-06 10:11:00 -0300

    exercicio 8

---
+++ exercicio8.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 8</title>
+    <style>
+        span{
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo "<h2>PPPPPP<br></h2>";
+        echo "<h2>P<span>PPPP</span>P</h2>";
+        echo "<h2>P<span>PPPP</span>P</h2>";
+        echo "<h2>PPPPPP</h2>";
+        echo "<h2>P</h2>";
+        echo "<h2>P</h2>";
+        echo "<h2>P</h2>";
+        echo "<h2>P</h2>";
+    ?>
+</body>
+</html>
