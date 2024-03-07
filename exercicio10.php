0a56b5ac5c38a85eb85159ef16f8e60876ef826e
João Souto <joaosouto0@outlook.com>
2024-03-06 22:15:28 -0300

    conclusão

--- exercicio10.php
+++ exercicio10.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 10</title>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <?php
         $x = "x";
-        $x = "x";
+        for ($i = 0; $i <= 20; $i++) { 
+            echo "$x<br>";
+            $x = $x . "xx";
+        }
+        echo "<p>XXXX</p>";
+        echo "<p>XXXX</p>";
+        echo "<p>XXXX</p>";
+        echo "<p>XXXX</p>";
+        echo "<p>XXXX</p>";
+        echo "<p>XXXX</p>";
     ?>
 </body>
 </html>
