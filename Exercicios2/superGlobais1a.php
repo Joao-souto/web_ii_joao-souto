f476ee955dcabb9bc61d69556ba260290a245157
João Souto <joaosouto0@outlook.com>
2024-03-27 10:15:58 -0300

    update

---
+++ Exercicios2/superGlobais1a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 1</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <fieldset>
+            <legend>Atividade 1 a</legend>
+        </fieldset>
+
+        <label for="num1">Insira o número 1:</label>
+        <input type="number" id="num1" name="num1" />
+    </form>
+</body>
+
+</html>
