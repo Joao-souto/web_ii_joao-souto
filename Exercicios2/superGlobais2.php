bb5aa7ecfc706f73771c50c707f8b319bc1dfced
João Souto <joaosouto0@outlook.com>
2024-03-30 20:57:29 -0300

    atividade 2

---
+++ Exercicios2/superGlobais2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 2</title>
+</head>
+<body>
+    
+    <form action="superGlobais2.php" method="post">
+        <fieldset>
+            <legend>Cadastro</legend>
+
+            <label for="nome">Nome: </label>
+            <input type="text" id="nome" name="nome"><br><br>
+
+            <label for="idade">Idade: </label>
+            <input type="number" id="idade" name="idade"><br><br>
+
+            <label for="genero">Gênero: </label><br>
+            <input type="radio" name="genero" value="masculino">masculino<br>
+            <input type="radio" name="genero" value="feminino">Feminino<br><br>
+
+            <label for="linguagens">Linguagens favoritas: </label><br>
+            <input type="checkbox" name="linguagens[]" value="Java">Java<br>
+            <input type="checkbox" name="linguagens[]" value="JavaScript">JavaScript<br>
+            <input type="checkbox" name="linguagens[]" value="C">C<br><br>
+
+            <input type="submit" value="Enviar">
+
+        </fieldset>
+
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                if(isset($_POST["linguagens"])){
+                    echo "Olá, " . $_POST["nome"] . "! Registramos que você têm " . $_POST["idade"] . " anos, é do gênero " . $_POST["genero"] . " e você gosta de " . implode(", ", $_POST["linguagens"]) . ".";
+                }
+                else{
+                    echo "Olá, " . $_POST["nome"] . "! Registramos que você têm " . $_POST["idade"] . " anos, é do gênero " . $_POST["genero"] . " e você não gosta de nenhuma das linguagens acima."; 
+                }
+            }
+        ?>
+    </form>
+    
+</body>
+</html>
