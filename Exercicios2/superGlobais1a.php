21e825a0aaff5797a23ceb8046b413783956e085
João Souto <joaosouto0@outlook.com>
2024-03-30 19:49:21 -0300

    update

--- Exercicios2/superGlobais1a.php
+++ Exercicios2/superGlobais1a.php
@@ -15,14 +15,35 @@
             
             <label for="num1">Insira o número 1:</label>
             <input type="number" id="num1" name="num1" />
+            <br>
             
             <label for="num2">Insira o número 2:</label>
             <input type="number" id="num2" name="num2" />
+            <br>
+            <input type="submit" value="Calcular">
+            <br>
+
+            <?php
+            //acessando o valor que o usuário inserir atráves da superglobal $_GET 
+            if(isset($_GET['num1']) && isset($_GET['num2'])){
+                $num1 = $_GET['num1'];
+                $num2 = $_GET['num2'];
+
+                echo "Soma: " . (num1 + num2) . ".<br>";
+                echo "Subtração: " . (num1 - num2) . ".<br>";
+                echo "Multiplicação: " . (num1 * num2) . ".<br>";
+
+                if(num2 != 0){
+                    echo "Divisão: " . (num1/num2) . ".<br>";
+                }
+                else{
+                    echo "A divisão não pode ser por 0!!"
+                }
+            }
+            ?>
         </fieldset>
 
-        <?php
-            
-        ?>
+
 
     </form>
 </body>
